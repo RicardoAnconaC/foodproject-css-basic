aa28475f6319d726ad7a6f8b9a46be9047716114
RicardoAnconaC <ricardo.ancona22@gmail.com>
2020-09-08 20:32:25 -0500

    Initialize homepage

---
+++ shared.js
@@ -0,0 +1,21 @@
+// var header = document.querySelector(".main-header");
+// var backdrop = document.querySelector(".backdrop");
+// var toggleButton = document.querySelector(".toggle-button");
+// var toggleNav = document.querySelector("#toggle-nav");
+// var mobileNav = document.querySelector(".mobile-nav");
+
+// backdrop.addEventListener("click", close_navbar);
+
+// toggleButton.addEventListener("click", function() {
+//     header.style.display = 'none';
+//     mobileNav.style.display = 'block';
+//     backdrop.style.display = 'block';
+// });
+
+// toggleNav.addEventListener("click", close_navbar);
+
+// function close_navbar() {
+//     header.style.display = 'block';
+//     mobileNav.style.display = 'none';
+//     backdrop.style.display = 'none';
+// }
