e416259271982f36d878e9321ff93221c8ac2eda
RicardoAnconaC <ricardo.ancona22@gmail.com>
2020-09-12 16:17:27 -0500

    Finish sharedJS

--- shared.js
+++ shared.js
@@ -1,45 +1,19 @@
 var backdrop = document.querySelector(".backdrop");
-var modal = document.querySelector(".modal");
-var modalNoButton = document.querySelector(".modal__action--negative");
-var selectPlanButtons = document.querySelectorAll(".plan button");
-var toggleButton = document.querySelector(".toggle-button");
+var toggleHome = document.querySelector("#toggle-home");
+var toggleNav = document.querySelector("#toggle-nav");
 var mobileNav = document.querySelector(".mobile-nav");
 
-// console.dir(backdrop.style['background-image']);
-
-// console.dir(backdrop);
-for (var i = 0; i < selectPlanButtons.length; i++) {
-  selectPlanButtons[i].addEventListener("click", function() {
-    // modal.style.display = "block";
-    // backdrop.style.display = "block";
-    // modal.className = 'open'; // This will actually overwrite the complete class list
-    modal.classList.add("open");
-    backdrop.classList.add("open");
-  });
-}
-
 backdrop.addEventListener("click", function() {
-  // mobileNav.style.display = 'none';
-  mobileNav.classList.remove("open");
-  closeModal();
+  mobileNav.style.display = 'none';
+  backdrop.style.display = "none";
 });
 
-if (modalNoButton) {
-  modalNoButton.addEventListener("click", closeModal);
-}
+toggleNav.addEventListener("click", function() {
+  mobileNav.style.display = 'none';
+  backdrop.style.display = "none";
+});
 
-function closeModal() {
-  //   backdrop.style.display = "none";
-  //   modal.style.display = "none";
-  if (modal) {
-    modal.classList.remove("open");
-  }
-  backdrop.classList.remove("open");
-}
-
-toggleButton.addEventListener("click", function() {
-  // mobileNav.style.display = 'block';
-  // backdrop.style.display = 'block';
-  mobileNav.classList.add("open");
-  backdrop.classList.add("open");
+toggleHome.addEventListener("click", function() {
+  mobileNav.style.display = 'block';
+  backdrop.style.display = 'block';
 });
