80f8d66d62b790f559bc4e30190b42142083fab6
RicardoAnconaC <ricardo.ancona22@gmail.com>
2020-09-15 19:28:35 -0500

    Add animated navbar

--- shared.js
+++ shared.js
@@ -4,16 +4,25 @@ var toggleNav = document.querySelector("#toggle-nav");
 var mobileNav = document.querySelector(".mobile-nav");
 
 backdrop.addEventListener("click", function() {
-  mobileNav.style.display = 'none';
-  backdrop.style.display = "none";
+  mobileNav.classList.remove("open");
+  backdrop.classList.remove("open");
+  setTimeout(function() {
+    backdrop.style.display = "none";
+  }, 200);
 });
 
 toggleNav.addEventListener("click", function() {
-  mobileNav.style.display = 'none';
-  backdrop.style.display = "none";
+  mobileNav.classList.remove("open");
+  backdrop.classList.remove("open");
+  setTimeout(function() {
+    backdrop.style.display = "none";
+  }, 200);
 });
 
 toggleHome.addEventListener("click", function() {
-  mobileNav.style.display = 'block';
-  backdrop.style.display = 'block';
+  mobileNav.classList.add("open");
+  backdrop.style.display = "block";
+  setTimeout(function() {
+    backdrop.classList.add("open");
+  }, 10);
 });
